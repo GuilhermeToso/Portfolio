76b1ecb4aded27a84a158dd023c91542629802c8
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-09 16:08:46 -0300

    Update Nav component

--- src/Components/Molecules/Nav/Nav.tsx
+++ src/Components/Molecules/Nav/Nav.tsx
@@ -1,13 +1,12 @@
-import { Container, Tabs} from '@mui/material'
-import React from 'react'
+import { Container } from '@mui/material'
+import React, {useContext} from 'react'
 import NavLink from '../../Atoms/NavLink/NavLink'
 import { NavGrid, NavTabs } from './Nav.styles'
 import {useTranslation} from "next-i18next"
-import {useRouter, NextRouter} from "next/router"
+import { NextRouter} from "next/router"
+import { NavigationContext } from '../../../Contexts'
 
 interface NavProps {
-    tabValue: number;
-    setTabValue: React.Dispatch<React.SetStateAction<number>>;
     router: NextRouter;
 }
 
@@ -16,12 +15,14 @@ const Nav = (props: NavProps) => {
     const {t} = useTranslation('common')
     const router = props.router
 
-    const {tabValue, setTabValue} = props
+    const {tabValue, handleTabValue} = useContext(NavigationContext)
+    
     
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setTabValue(newValue);
+        handleTabValue(newValue);
     };
 
+    console.log(tabValue)
 
     return (
         <NavGrid item container justifyContent="center" justifyItems="center" md={8} xs={0}>
@@ -31,12 +32,12 @@ const Nav = (props: NavProps) => {
                  onChange={handleChange}
                  aria-label="scrollable prevent tabs example"
                 >
-                    <NavLink to="home" label={router.locale == "en-US" ? t("home") : t("begin")} state={0} setTabValue={setTabValue}></NavLink>
-                    <NavLink to="about" label={t("about")} state={1} setTabValue={setTabValue}></NavLink>
-                    <NavLink to="skills" label={t("skills")} state={2} setTabValue={setTabValue}></NavLink>
-                    <NavLink to="services" label={t("services")} state={3} setTabValue={setTabValue}></NavLink>
-                    <NavLink to="portfolio" label={t("portfolio")} state={4} setTabValue={setTabValue}></NavLink>
-                    <NavLink to="contact" label={t("contact")} state={5} setTabValue={setTabValue}></NavLink>
+                    <NavLink to="home" label={router.locale == "en-US" ? t("home") : t("begin")} state={0}></NavLink>
+                    <NavLink to="about" label={t("about")} state={1}></NavLink>
+                    <NavLink to="skills" label={t("skills")} state={2}></NavLink>
+                    <NavLink to="services" label={t("services")} state={3}></NavLink>
+                    <NavLink to="portfolio" label={t("portfolio")} state={4}></NavLink>
+                    <NavLink to="contact" label={t("contact")} state={5}></NavLink>
                 </NavTabs>
             </Container>
         </NavGrid>
