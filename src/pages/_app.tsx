a65561ffb46fb32ccfc6ef980d146f00756e23e9
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-10 14:55:45 -0300

    Add useEffect hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
+import React, {useEffect} from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import {appWithTranslation} from "next-i18next"
+import AOS from "aos"
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: false,
+      offset: 50,
+    })
+  }, [])
+  
+
   return <Component {...pageProps} />
 }
 
