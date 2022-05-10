9170ce508e242b7ac60462e975e145ecda7c8610
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-10 14:56:20 -0300

    Remove AOS link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600&display=swap" rel="stylesheet" /> 
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
+        
       </Head>
       <body>
         <Main />
