cf70ad374c5a7bffc9c1b2ded180ebc69087de86
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-10 11:22:05 -0300

    Created AboutDescription component

---
+++ src/Components/Molecules/AboutDescription/AboutDescription.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Grid, Typography} from "@mui/material"
+
+const AboutDescription = () => {
+  return (
+    <Grid item xs={12} md={6}>
+        <Typography>
+            Right Side
+        </Typography>
+    </Grid>
+  )
+}
+
+export default AboutDescription
