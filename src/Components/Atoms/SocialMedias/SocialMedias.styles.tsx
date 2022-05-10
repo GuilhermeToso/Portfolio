add2ecef31a8bedcdc96ad70ffe094fbf9405fe9
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-10 11:21:11 -0300

    Create social media and icon styles

--- src/Components/Atoms/SocialMedias/SocialMedias.styles.tsx
+++ src/Components/Atoms/SocialMedias/SocialMedias.styles.tsx
@@ -13,3 +13,16 @@ export const styles: MotionStyle = {
     height:"100%", 
 }
 
+export const socialMediaStyles ={
+    zIndex:"1"
+}
+
+export const iconsStyle = {
+    fontSize:"36px",
+    '@media (max-width:900px)':{
+        fontSize:"27px"
+    },
+    '@media (max-height: 568px) and (min-width:300px)':{
+        fontSize:"24px"
+    }
+}
