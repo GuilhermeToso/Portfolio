e912fdcf1c6a81c5d22761e7cb46ca728e433110
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-11 14:30:49 -0300

    Remove imports and comments

--- src/Components/Molecules/LanguageButton/LanguageButton.tsx
+++ src/Components/Molecules/LanguageButton/LanguageButton.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { LanguageGrid, IconButton, LanguageIcon } from './LanguageButton.styles'
-import { LanguagesProps } from '../../Organisms/Header/Header'
-
 interface LanguageButtonProps {
     event: (event: React.MouseEvent<HTMLButtonElement>) => void
     open: boolean
@@ -13,9 +11,6 @@ const LanguageButton = (props: LanguageButtonProps) => {
         <IconButton 
             disableRipple
             onClick={props.event}
-            // aria-controls={props.open ? 'basic-menu' : undefined}
-            // aria-haspopup="true"
-            // aria-expanded={props.open ? 'true' : undefined}
         >
             <LanguageIcon ></LanguageIcon>
         </IconButton>
