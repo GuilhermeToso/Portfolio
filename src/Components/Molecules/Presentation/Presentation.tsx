69f5490594586cf5374f8e4bba5fce3e48f00942
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-11 14:32:24 -0300

    Remove imports

--- src/Components/Molecules/Presentation/Presentation.tsx
+++ src/Components/Molecules/Presentation/Presentation.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { Description } from '../../Atoms/Description'
 import { Introduction } from '../../Atoms/Introduction'
 import { Profession } from '../../Atoms/Profession'
@@ -6,7 +6,6 @@ import { Salution } from '../../Atoms/Salution'
 import { PresentationGrid} from './Presentation.styles'
 import { SocialMedias } from '../../Atoms/SocialMedias'
 import {CTAbuttons} from '../../Atoms/CTAbuttons'
-import { breakpoint } from '../../../Constants'
 
 const Presentation = () => {
 
