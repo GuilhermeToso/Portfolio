91a08f007a0e9876d26abc3c0f981745463225dd
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-11 14:33:41 -0300

    Remove imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { Template } from '../Components/Templates'
 import {serverSideTranslations} from "next-i18next/serverSideTranslations"
 
