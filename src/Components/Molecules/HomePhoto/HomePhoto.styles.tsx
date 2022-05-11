3203149ee3d3b82f1775797e850e6c99dea1941b
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-11 14:29:50 -0300

    Remove HomePhotoBox styled component

--- src/Components/Molecules/HomePhoto/HomePhoto.styles.tsx
+++ src/Components/Molecules/HomePhoto/HomePhoto.styles.tsx
@@ -11,26 +11,6 @@ export const style: MotionStyle = {
     height:"100%",
   }
 
-export const HomePhotoBox = styled(Box,{})({
-    position:'relative',
-    left:"0vw", 
-    width:"50%", 
-    height:"100%",
-    '@media (max-width:900px)':{
-      width:"100%",
-      top:"-10%",
-      left:"0%",
-      height:"65%",
-    },
-    '@media (max-height: 568px) and (min-width:300px)':{
-      width:"100%",
-      top:"15%",
-      left:"-25%",
-      height:"85%",
-    },
-
-})
-
 export const HomePhotoGrid = styled(Grid,{})({
   position:'relative',
   left:"0vw", 
