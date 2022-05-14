44595b18c0dbbf31efc4f2c7d53781473552d0f9
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:19:57 -0300

    Add styles for Home page

---
+++ src/Components/Organisms/Home/Home.styles.tsx
@@ -0,0 +1,9 @@
+export const homeStyles = {
+    flexDirection:"row",
+    '@media (max-width:900px)':{
+        flexDirection:"column"
+    },
+    '@media (max-height:568px) and (min-width:300px)':{
+        flexDirection:"row"
+    }
+}
