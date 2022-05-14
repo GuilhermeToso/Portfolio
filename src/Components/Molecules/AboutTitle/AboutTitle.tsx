88f3c540acadf3d59800405317a798ae6197164c
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:12:49 -0300

    Update AboutTitle component

--- src/Components/Molecules/AboutTitle/AboutTitle.tsx
+++ src/Components/Molecules/AboutTitle/AboutTitle.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Divider, Typography } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import { AboutTitleGrid, typographyStyle } from './AboutTitleGrid.styles'
 import { theme } from '../../../styles'
@@ -9,15 +9,16 @@ const AboutTitle = () => {
   const {t} = useTranslation("about")
   return (
     <AboutTitleGrid item xs={12} container justifyContent="center">
-      <div data-aos="flip-right">
+      <div data-aos="fade-down" data-aos-duration="500">
         <Typography
           fontFamily={theme.typography.fontFamily.title}
           fontWeight="600"
           sx={typographyStyle}
-          color="#333"
+          color={theme.palette.grey.A900}
         >
           {t("title")}
         </Typography>
+        <Divider/>
       </div>
       
     </AboutTitleGrid>
