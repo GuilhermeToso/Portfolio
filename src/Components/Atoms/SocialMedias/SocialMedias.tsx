c37df579ff489d0998e8cb842a40334915940879
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:09:56 -0300

    Update SocialMedias component

--- src/Components/Atoms/SocialMedias/SocialMedias.tsx
+++ src/Components/Atoms/SocialMedias/SocialMedias.tsx
@@ -1,77 +1,56 @@
 import React from 'react'
-import { SocialMediasBox, styles, iconsStyle, socialMediaStyles } from './SocialMedias.styles'
-import { motion } from 'framer-motion'
+import { iconsStyle, socialMediaStyles, presentationStyle } from './SocialMedias.styles'
 import { ButtonGroup, IconButton} from '@mui/material'
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { PresentationElement } from '../../Styles';
 
 
 const SocialMedias = () => {
 
-  const variations = {
-      hidden:{
-          opacity: 0,
-          left:"0%"
-      },
-      visible:{
-        opacity: 1,
-        left:"15%",
-        transition:{
-            delay:1,
-            duration:1
-        }
-    }
-  }
-  
   return (
-    <SocialMediasBox>
-        <motion.div
-            variants={variations}
-            initial="hidden"
-            animate="visible"
-            style={styles}
+    <PresentationElement
+      sx={presentationStyle}
+    >
+      <ButtonGroup
+        size='large'
+      >
+        <IconButton
+          aria-label="linkedin"
+          size='large'
+          sx={socialMediaStyles}
+          href='https://www.linkedin.com/in/guilherme-toso/'
         >
-
-            <ButtonGroup
-              size='large'
-            >
-              <IconButton
-                aria-label="linkedin"
-                size='large'
-                sx={socialMediaStyles}
-                href='https://www.linkedin.com/in/guilherme-toso/'
-              >
-                <LinkedInIcon sx={iconsStyle}/>
-              </IconButton>
-              <IconButton
-                aria-label="github repository"
-                size='large'
-                sx={socialMediaStyles}
-                href='https://github.com/GuilhermeToso'
-              >
-                <GitHubIcon sx={iconsStyle}/>
-              </IconButton>
-              <IconButton
-                aria-label="instagram"
-                size='large'
-                sx={socialMediaStyles}
-                href="https://www.instagram.com/guilhermetoso/"
-              >
-                <InstagramIcon sx={iconsStyle}/>
-              </IconButton>
-              <IconButton
-                aria-label="facebook"
-                size='large'
-                sx={socialMediaStyles}
-                href="https://www.facebook.com"
-              >
-                <FacebookOutlinedIcon sx={iconsStyle}/>
-              </IconButton>
-            </ButtonGroup>
-        </motion.div>
-    </SocialMediasBox>
+          <LinkedInIcon sx={iconsStyle}/>
+        </IconButton>
+        <IconButton
+          aria-label="github repository"
+          size='large'
+          sx={socialMediaStyles}
+          href='https://github.com/GuilhermeToso'
+        >
+          <GitHubIcon sx={iconsStyle}/>
+        </IconButton>
+        <IconButton
+          aria-label="instagram"
+          size='large'
+          sx={socialMediaStyles}
+          href="https://www.instagram.com/guilhermetoso/"
+        >
+          <InstagramIcon sx={iconsStyle}/>
+        </IconButton>
+        <IconButton
+          aria-label="facebook"
+          size='large'
+          sx={socialMediaStyles}
+          href="https://www.facebook.com"
+        >
+          <FacebookOutlinedIcon sx={iconsStyle}/>
+        </IconButton>
+      </ButtonGroup>
+    </PresentationElement>
   )
 }
 
