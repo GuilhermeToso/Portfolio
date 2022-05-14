c53cb94cc9af9229a56bccb73e1d01222dfe8a0c
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:13:59 -0300

    Update HomePhoto styles

--- src/Components/Molecules/HomePhoto/HomePhoto.styles.tsx
+++ src/Components/Molecules/HomePhoto/HomePhoto.styles.tsx
@@ -1,35 +1,26 @@
 import styled from "@emotion/styled";
-import { Box, Grid } from "@mui/material";
-import { MotionStyle } from "framer-motion";
-
-
-
-  
-export const style: MotionStyle = {
-    position:'relative', 
-    width:"100%", 
-    height:"100%",
-  }
+import { Grid } from "@mui/material";
 
 export const HomePhotoGrid = styled(Grid,{})({
   position:'relative',
-  left:"0vw", 
-  width:"50%", 
+  // display:"flex",
+  flex:"6",
+  width:"100%", 
   height:"100%",
   '@media (max-width:900px)':{
     width:"100%",
-    height:"59%",
+    height:"50%",
+    flex:"12"
   },
   '@media (max-height: 568px) and (min-width:300px)':{
     width:"50%",
-    top:"0%",
-    left:"10%",
     height:"100%",
+    flex:"6"
   },
 
 })
 
-export const HomePhotoContainer = styled(Box,{})({
+export const HomePhotoDiv = styled('div')({
     position:"relative",
     width:"100%",
     height:"100%",
