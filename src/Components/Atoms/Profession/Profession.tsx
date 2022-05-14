b58db271fb6bc631f16ea1fb38f4b458305550dc
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:07:47 -0300

    Update rofession component

--- src/Components/Atoms/Profession/Profession.tsx
+++ src/Components/Atoms/Profession/Profession.tsx
@@ -1,53 +1,29 @@
 import React from 'react'
-import { ProfessionBox, typographyStyles } from './Profession.styles'
+import { typographyStyles } from './Profession.styles'
 import { useTranslation } from 'next-i18next'
-import { motion, MotionStyle } from 'framer-motion'
 import { Typography } from '@mui/material'
 import { theme } from '../../../styles'
+import {PresentationElement} from "../../Styles"
 
 const Profession = () => {
-
-  const variations = {
-      hidden:{
-          opacity: 0,
-          left:"0%"
-      },
-      visible:{
-        opacity: 1,
-        left:"15%",
-        transition:{
-            delay:0.6,
-            duration:1
-        }
-    }
-  }
-
-  const styles: MotionStyle = {
-    position:"relative",
-    width:"85%", 
-    height:"100%"
-}
 
   const {t} = useTranslation("home")
   
   return (
-    <ProfessionBox>
-        <motion.div
-            variants={variations}
-            initial="hidden"
-            animate="visible"
-            style={styles}
-        >
-          <Typography
-            fontFamily={theme.typography.fontFamily.title}
-            color={theme.palette.grey.A800}
-            sx={typographyStyles}
-            align="left"
-          >
-            {t("profession")}
-          </Typography>
-        </motion.div>
-    </ProfessionBox>
+    <PresentationElement
+      data-aos="fade-right"
+      data-aos-duration="1000"
+      data-aos-delay="400"
+    >
+      <Typography
+        fontFamily={theme.typography.fontFamily.title}
+        color={theme.palette.grey.A800}
+        sx={typographyStyles}
+        align="left"
+      >
+        {t("profession")}
+      </Typography>
+    </PresentationElement>
   )
 }
 
