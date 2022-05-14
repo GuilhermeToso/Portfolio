926e4dcbb29b1ae0539b1b86ff0a24ff9317d68e
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:03:59 -0300

    Update CTAbuttons animation

--- src/Components/Atoms/CTAbuttons/CTAbuttons.tsx
+++ src/Components/Atoms/CTAbuttons/CTAbuttons.tsx
@@ -1,78 +1,60 @@
 import React, {useContext} from 'react'
-import { CTAbuttonsBox, styles, CTAButton, CTAButtonText, buttonStyle } from './CTAbuttons.styles'
+import { CTAButton, CTAButtonText, buttonStyle, presentationStyle } from './CTAbuttons.styles'
 import { useTranslation } from 'next-i18next'
-import { motion } from 'framer-motion'
 import DownloadIcon from '@mui/icons-material/Download';
 import { theme } from '../../../styles';
 import { Link } from "react-scroll"
 import { NavigationContext } from '../../../Contexts';
+import { PresentationElement } from '../../Styles';
+import { ButtonGroup } from '@mui/material';
+
 
 const CTAbuttons = () => {
 
-  const variations = {
-      hidden:{
-          opacity: 0,
-          left:"0%"
-      },
-      visible:{
-        opacity: 1,
-        left:"15%",
-        transition:{
-            delay:1.2,
-            duration:1
-        }
-    }
-  }
-  
   const {t} = useTranslation("home")
   const {handleTabValue} = useContext(NavigationContext)
 
   return (
-    <CTAbuttonsBox>
-        <motion.div
-            variants={variations}
-            initial="hidden"
-            animate="visible"
-            style={styles}
+    <PresentationElement 
+      sx={presentationStyle}   
+    >
+      <CTAButton
+        variant="outlined"
+        endIcon={<DownloadIcon></DownloadIcon>}
+        href="/documents/cv.pdf"
+        sx={buttonStyle}
+      >
+        <CTAButtonText
+          fontFamily={theme.typography.fontFamily.title}
+          fontWeight="600"
+          align='center'
         >
-            <CTAButton
-              variant="outlined"
-              endIcon={<DownloadIcon></DownloadIcon>}
-              href="/documents/cv.pdf"
-              sx={buttonStyle}
-            >
-              <CTAButtonText
-                fontFamily={theme.typography.fontFamily.title}
-                fontWeight="600"
-                align='center'
-              >
-                {t("cta_button")}
-              </CTAButtonText>
-            </CTAButton>
-            <div style={{width:"10px"}}></div>
-            <CTAButton
-              variant="contained"
-              sx={buttonStyle}
-            >
-              <Link
-                to="about"
-                spy={true} 
-                smooth={true} 
-                offset={0} 
-                duration={500}
-                onClick={()=>{handleTabValue(1)}}
-              >
-                <CTAButtonText
-                  fontFamily={theme.typography.fontFamily.title}
-                  fontWeight="600"
-                  color="#fff"
-                >
-                  {t("lets_talk")}
-                </CTAButtonText>
-              </Link>
-            </CTAButton>
-        </motion.div>
-    </CTAbuttonsBox>
+          {t("cta_button")}
+        </CTAButtonText>
+      </CTAButton>
+      <div style={{width:"10px"}}></div>
+      <CTAButton
+        variant="contained"
+        sx={buttonStyle}
+      >
+        <Link
+          to="about"
+          spy={true} 
+          smooth={true} 
+          offset={0} 
+          duration={500}
+          onClick={()=>{handleTabValue(1)}}
+        >
+          <CTAButtonText
+            fontFamily={theme.typography.fontFamily.title}
+            fontWeight="600"
+            color="#fff"
+          >
+            {t("lets_talk")}
+          </CTAButtonText>
+        </Link>
+      </CTAButton>
+    </PresentationElement>
   )
 }
 
