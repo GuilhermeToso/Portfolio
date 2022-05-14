caf1b7f7a2eb4987397a4080d31fba5e6f8b4988
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:22:45 -0300

    Create PresentationElement styled component

---
+++ src/Components/Styles/index.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+
+export const PresentationElement = styled(Box,{})({
+    position:"relative",
+    width:"85%",
+    left:"15%"
+})
