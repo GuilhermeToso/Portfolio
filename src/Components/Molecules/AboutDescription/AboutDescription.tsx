24d6c19adeec2bee35ffb636af5a7fbc32353c30
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:11:16 -0300

    Update AboutDescription components

--- src/Components/Molecules/AboutDescription/AboutDescription.tsx
+++ src/Components/Molecules/AboutDescription/AboutDescription.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import {Grid, Typography} from "@mui/material"
+import {Grid, Stack, Typography, Paper} from "@mui/material"
+import { TextDiv, ExperienceDiv } from './AboutDescription.styles'
+import { useTranslation } from "next-i18next"
+import { theme } from '../../../styles'
 
 const AboutDescription = () => {
+
+  const {t} = useTranslation("about")
+
   return (
-    <Grid item xs={12} md={6}>
-        <Typography>
-            Right Side
+    <Grid item container xs={12} md={5.75}>
+      <TextDiv data-aos="fade-up" data-aos-duration="1000">
+        <Typography
+          fontFamily={theme.typography.fontFamily.text}
+          fontWeight="400"
+          color={theme.palette.grey.A800}
+          align="left"
+        >
+
+          {t("about_me")}
         </Typography>
+      </TextDiv>
+      <ExperienceDiv data-aos="fade-up" data-aos-duration="1000">
+        <Stack direction="row" spacing={3}>
+            <Paper elevation={14}>
+              Hi
+            </Paper>
+            <Paper elevation={14}>
+              Hi
+            </Paper>
+        </Stack>
+      </ExperienceDiv>
     </Grid>
   )
 }
