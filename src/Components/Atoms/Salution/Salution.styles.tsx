2e3dc0d1592b64250985dcb43dfffc0c02c9391c
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:08:12 -0300

    Remove SalutionBox styled component

--- src/Components/Atoms/Salution/Salution.styles.tsx
+++ src/Components/Atoms/Salution/Salution.styles.tsx
@@ -1,13 +1,6 @@
-import styled from "@emotion/styled";
-import { Box } from "@mui/material";
 import {theme} from "../../../styles"
 
 const fontSizes = theme.typography.fontSizes
-
-export const SalutionBox = styled(Box,{})({
-    position:"relative",
-    width:"100%",
-})
 
 export const typographyStyles = {
     fontSize:fontSizes.xxs
