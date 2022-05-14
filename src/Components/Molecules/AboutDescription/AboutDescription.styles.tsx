eeca1ae8f42cdb3e346dee7fa6ee6fc4eac0ab4c
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:10:25 -0300

    Add About styled components

---
+++ src/Components/Molecules/AboutDescription/AboutDescription.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const ExperienceDiv = styled('div')({
+    margin:"10% 40% 0 0",
+    '@media (max-width:900px)':{
+        margin:"5% 10% 0 10%"
+    }
+})
+
+export const TextDiv = styled('div')({
+    margin:"10% 40% 0 0",
+    '@media (max-width:900px)':{
+        margin:"5% 10% 0 10%"
+    }
+})
