0014f5b6349d3616d4a3fcc8cd8ffce6745676e2
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:14:34 -0300

    Update HomePhoto component

--- src/Components/Molecules/HomePhoto/HomePhoto.tsx
+++ src/Components/Molecules/HomePhoto/HomePhoto.tsx
@@ -1,44 +1,25 @@
 import React from 'react'
-import { motion } from 'framer-motion'
-import { HomePhotoContainer, style, HomePhotoGrid } from './HomePhoto.styles'
+import { HomePhotoDiv, HomePhotoGrid } from './HomePhoto.styles'
 import Image from 'next/image'
 
 
 
 const HomePhoto = () => {
 
-  const homePhotoVariants = {
-    initial: {
-      left: "50vw" ,
-      opacity:"0"
-    },
-    animate: {
-      opacity:1,
-      left:"0vw",
-      transition: {
-        delay: 0.3, duration:2
-      }
-    }
-  }
-
   return (
-    <HomePhotoGrid item md={6} xs={12}>
-      <motion.div
-        variants={homePhotoVariants}
-        initial="initial"
-        animate="animate"
-      style={style}
+    <HomePhotoGrid item>
+      <HomePhotoDiv
+        data-aos="fade-left"
+        data-aos-duration="2000"
       >
-        <HomePhotoContainer>
-          <Image
-              src="/images/me-bw.png"
-              alt="home-me"
-              layout='fill'
-              objectFit='contain'
-          >
-          </Image>
-        </HomePhotoContainer>
-      </motion.div>
+        <Image
+            src="/images/me-bw.png"
+            alt="home-me"
+            layout='fill'
+            objectFit='contain'
+        >
+        </Image>
+      </HomePhotoDiv>
     </HomePhotoGrid>
     
   )
