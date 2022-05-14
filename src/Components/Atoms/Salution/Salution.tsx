ae7baa09bc1a1f85f92c261cebd090691de7f2a8
GuilhermeToso <guilhermetoso@hotmail.com>
2022-05-14 11:08:37 -0300

    Update Salution component

--- src/Components/Atoms/Salution/Salution.tsx
+++ src/Components/Atoms/Salution/Salution.tsx
@@ -1,47 +1,28 @@
 import React from 'react'
-import { SalutionBox, typographyStyles } from './Salution.styles'
+import { typographyStyles } from './Salution.styles'
 import { useTranslation } from 'next-i18next'
-import { motion } from 'framer-motion'
 import {Typography} from "@mui/material"
 import { theme } from '../../../styles'
+import { PresentationElement } from '../../Styles'
 
 const Salution = () => {
-
-  const variations = {
-      hidden:{
-          opacity: 0,
-          left:"0%"
-      },
-      visible:{
-        opacity: 1,
-        left:"15%",
-        transition:{
-            delay:0.2,
-            duration:1
-        }
-    }
-  }
 
   const {t} = useTranslation("home")
   
   return (
-    <SalutionBox>
-        <motion.div
-            variants={variations}
-            initial="hidden"
-            animate="visible"
-            style={{position:"relative", width: '85%'}}
-        >
-          <Typography
-            fontFamily={theme.typography.fontFamily.title}
-            color={theme.palette.grey.A800}
-            align="left"
-            sx={typographyStyles}
-          >
-            {t("salution")}
-          </Typography>
-        </motion.div>
-    </SalutionBox>
+    <PresentationElement 
+      data-aos="fade-right" 
+      data-aos-duration="1000"
+    >
+      <Typography
+        fontFamily={theme.typography.fontFamily.title}
+        color={theme.palette.grey.A800}
+        align="left"
+        sx={typographyStyles}
+      >
+        {t("salution")}
+      </Typography>
+    </PresentationElement>
   )
 }
 
